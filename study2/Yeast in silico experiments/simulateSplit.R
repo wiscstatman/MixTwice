48e16d287ea1104c1e7690952ec59353e244c194
zihao zheng <64521902+ZihaoZheng-Stat@users.noreply.github.com>
2021-10-16 10:23:37 -0500

    update simulatesplit, only get the data (further calculation will avoid DEseq)

--- study2/Yeast in silico experiments/simulateSplit.R
+++ study2/Yeast in silico experiments/simulateSplit.R
@@ -3,13 +3,10 @@ simulateSplit = function(X,
                          rseed, 
                          nDE, 
                          sampleSize,
-                         uninformativeCovariate = FALSE, 
-                         pvalHists = FALSE,
-                         strongCovariate = TRUE){
+                         signal ## fold value with DE
+){
   
   dds_full <- dds_full[,colData(dds_full)$condition == group] ## make a subset group
-  
-  # set random seed
   
   set.seed(as.numeric(X)*as.numeric(rseed))
   
@@ -31,96 +28,33 @@ simulateSplit = function(X,
   
   truth <- rep(FALSE, nrow(dds_test))
   
-  # make sure null comparison is truly null
+  DE <- sample(1:nrow(dds_test), nDE, prob = covar)
+  truth[DE] <- TRUE
   
-  # if PC1 or PC2 sig different, or if test of join means of PCs 1:4 is significant, reshuffle sample labels
-  dds_test <- estimateSizeFactors(dds_test)
-  x <- t(counts(dds_test, normalize=TRUE))
-  pc <- prcomp(log(x + 0.5), scale.=TRUE)
+  ### I will have a signal vector of length length(truth)
   
-  a1 <- pc$x[colData(dds_test)$fake=="A",1]
-  b1 <- pc$x[colData(dds_test)$fake=="B",1] 
-  a2 <- pc$x[colData(dds_test)$fake=="A",2]
-  b2 <- pc$x[colData(dds_test)$fake=="B",2] 
-  p1 <- t.test(a1, b1)$p.value
-  p2 <- t.test(a2, b2)$p.value
-  tries <- 0
+  multiple = rep(1, length(truth))
   
-  while(p1 < 0.10 || p2 < 0.10 && tries < 10){
-    colData(dds_test)$fake <- sample(colData(dds_test)$fake, ncol(dds_test))
-    x <- t(counts(dds_test, normalize=TRUE))
-    pc <- prcomp(log(x + 0.5), scale.=TRUE)
-    
-    a1 <- pc$x[colData(dds_test)$fake=="A",1]
-    b1 <- pc$x[colData(dds_test)$fake=="B",1] 
-    a2 <- pc$x[colData(dds_test)$fake=="A",2]
-    b2 <- pc$x[colData(dds_test)$fake=="B",2] 
-    p1 <- t.test(a1, b1)$p.value
-    p2 <- t.test(a2, b2)$p.value
-    
-    tries <- tries + 1
-  }
+  multiple[DE] = signal
   
-  if(nDE > 0){
-    # pick random set of nDE genes to add signal to
-    DE <- sample(1:nrow(dds_test), nDE, prob = covar)
-    truth[DE] <- TRUE
-    
-    # randomly sample a log2FC from original FCs (without regard to DE)
-    counts_new <- counts(dds_test)
-    log2FC <- rep(0, nrow(dds_test))
-    
-    if(sampleSize == 5){
-      log2FC[DE] <- res_5$log2FoldChange[pzero < 0.5][DE]
-    }else if (sampleSize == 10){
-      log2FC[DE] <- res_10$log2FoldChange[pzero < 0.5][DE]
-    }else{
-      stop("Only sample sizes 5 and 10 are currently supported with pre-",
-           "computed fold changes for sampling from.")
-    }
-    
-    # randomize which condition is shifted up or down
-    ran <- runif(nrow(dds_test)) 
-    refcond <- ifelse(ran < 0.5, "A", "B")
-    down <- which(ran < 0.5)
-    
-    counts_new[down,colData(dds_test)$fake==unique(refcond[down])] <- 
-      counts(dds_test)[down, colData(dds_test)$fake==unique(refcond[down])] *
-      2^log2FC[down]
-    counts_new[-down,colData(dds_test)$fake==unique(refcond[-down])] <- 
-      counts(dds_test)[-down, colData(dds_test)$fake==unique(refcond[-down])] *
-      2^log2FC[-down]
-    
-    counts_new <- apply(counts_new, 2, as.integer)
-    
-    
-    counts(dds_test) <- counts_new
-  }
+  counts_new <- counts(dds_test)
   
-  # replace existing size factors 
-  dds_test <- estimateSizeFactors(dds_test)
+  # randomize which condition is shifted up or down
+  ran <- runif(nrow(dds_test)) 
+  refcond <- ifelse(ran < 0.5, "A", "B")
+  down <- which(ran < 0.5)
   
-  dds_test <- DESeq(dds_test, parallel = FALSE)
-  resTEST <- results(dds_test, name="fake_B_vs_A", independentFiltering = FALSE)
+  counts_new[down,colData(dds_test)$fake==unique(refcond[down])] <- 
+    counts(dds_test)[down, colData(dds_test)$fake==unique(refcond[down])] *
+    multiple[down]
+  counts_new[-down,colData(dds_test)$fake==unique(refcond[-down])] <- 
+    counts(dds_test)[-down, colData(dds_test)$fake==unique(refcond[-down])] *
+    multiple[-down]
   
-  geneExp <- tbl_df(data.frame(pval=resTEST$pvalue, 
-                               SE=resTEST$lfcSE,                 
-                               ind_covariate = covar,
-                               effect_size = resTEST$log2FoldChange, 
-                               test_statistic = resTEST$stat,
-                               qvalue = truth))
+  counts_new <- apply(counts_new, 2, as.integer)
   
-  if (uninformativeCovariate){
-    geneExp <- mutate(geneExp, ind_covariate = runif(length(covar)))
-  }else if(!strongCovariate){
-    geneExp <- mutate(geneExp, ind_covariate = pmin(1, abs(covar + rnorm(length(covar), 0, 0.25))))
-  }
-  
-  geneExp <-  geneExp %>% dplyr::filter(!is.na(pval))
-  
-  return(geneExp)
-  
+  return(list(data = counts_new,
+              truth = truth,
+              group =colData(dds_test)$fake,
+              covar = covar))
 }
-
-
-
